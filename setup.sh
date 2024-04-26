23c73bf76cfc44d6c24aa6acac014e2b23321798
Mike Glenn <219478+ilude@users.noreply.github.com>
2024-04-26 15:01:56 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
-parted /dev/sda -- mklabel msdos
-parted /dev/sda -- mkpart primary 1MB -8GB
-parted /dev/sda -- mkpart primary linux-swap -8GB 100%
+parted /dev/sdb -- mklabel msdos
+parted /dev/sdb -- mkpart primary 1MB -8GB
+parted /dev/sdb -- mkpart primary linux-swap -8GB 100%
 
-mkfs.ext4 -L nixos /dev/sda1
-mkswap -L swap /dev/sda2
-swapon /dev/sda2
+mkfs.ext4 -L nixos /dev/sdb1
+mkswap -L swap /dev/sdb2
+swapon /dev/sdb2
 mount /dev/disk/by-label/nixos /mnt
 nixos-generate-config --root /mnt
 
