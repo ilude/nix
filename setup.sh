dc1027f2b6ce6a2148296c65cd7c2726666e4131
Mike Glenn <219478+ilude@users.noreply.github.com>
2024-04-26 11:50:08 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
+parted /dev/sda -- mklabel msdos
+parted /dev/sda -- mkpart primary 1MB -8GB
+parted /dev/sda -- mkpart primary linux-swap -8GB 100%
+
 mkfs.ext4 -L nixos /dev/sda1
 mkswap -L swap /dev/sda2
 swapon /dev/sda2
