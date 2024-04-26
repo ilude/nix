c8512dc5a08b933d0b25a0a1e8b8491139db4dc4
Mike Glenn <219478+ilude@users.noreply.github.com>
2024-04-26 17:11:02 -0400

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,35 +1,16 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 { config, lib, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
-
-  # Use the GRUB 2 boot loader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+  imports = [ ./hardware-configuration.nix ];
 
   # Set your time zone.
   time.timeZone = "America/New_York";
   #time.timeZone = "America/Los_Angeles";
 
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
-
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.anvil = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "docker" "systemd-joural" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" "docker" "systemd-journal" ]; # Enable ‘sudo’ for the user.
     shell = pkgs.zsh;
     password = "";
     #packages = with pkgs; [];
@@ -46,6 +27,18 @@
       ];
     }
   ];
+
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkb.options in tty.
+  # };
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
@@ -72,22 +65,5 @@
   services.qemuGuest.enable = true;
   virtualisation.docker.enable = true;
 
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "23.11"; # Did you read the comment?
-
 }
