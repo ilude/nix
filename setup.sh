4bc05de66c045d090567bea996281e7bf7372b73
Mike Glenn <219478+ilude@users.noreply.github.com>
2024-04-26 16:11:45 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
-parted /dev/sdb -- mklabel msdos
-parted /dev/sdb -- mkpart primary 1MB -8GB
-parted /dev/sdb -- mkpart primary linux-swap -8GB 100%
+parted /dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi0 -- mklabel msdos
+parted /dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi0 -- mkpart primary 1MB -8GB
+parted /dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi0 -- mkpart primary linux-swap -8GB 100%
 
-mkfs.ext4 -L nixos /dev/sdb1
-mkswap -L swap /dev/sdb2
-swapon /dev/sdb2
+mkfs.ext4 -L nixos /dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi0-part1
+mkswap -L swap /dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi0-part2
+swapon /dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi0-part2
 mount /dev/disk/by-label/nixos /mnt
 nixos-generate-config --root /mnt
 
