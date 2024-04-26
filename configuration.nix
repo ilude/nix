f88cc7abd0f3fcb592fa425f6979b7e706f6db26
Mike Glenn <219478+ilude@users.noreply.github.com>
2024-04-26 11:17:43 -0400

    Create configuration.nix

---
+++ configuration.nix
@@ -0,0 +1,102 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      #<nixpkgs/nixos/modules/profiles/qemu-guest.nix>
+    ];
+
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+
+  # networking.hostName = "nixos"; # Define your hostname.
+  # Pick only one of the below networking options.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+  # Set your time zone.
+  # time.timeZone = "Europe/Amsterdam";
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkb.options in tty.
+  # };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.anvil = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "docker" "systemd-joural" ]; # Enable ‘sudo’ for the user.
+    shell = pkgs.zsh;
+    #password = "SuperSecretPasswordHere";
+    #packages = with pkgs; [];
+  };
+
+  programs.zsh.enable = true;
+
+  security.sudo.extraRules= [
+  { users = [ "anvil" ];
+     commands = [
+        { command = "ALL" ;
+          options= [ "NOPASSWD" ]; # "SETENV" # Adding the following could be a good idea
+        }
+      ];
+    }
+  ];
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    curl
+    docker
+    docker-buildx
+    docker-compose
+    #nvidia-docker
+    git
+    jq
+    yq
+    gnumake
+    tree
+    python3
+    python3Packages.pip
+    ssh-import-id
+    wget
+  ];
+
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+  services.qemuGuest.enable = true;
+  virtualisation.docker.enable = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+}
