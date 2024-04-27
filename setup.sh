73e6af49a9ca710ecce035db2a492c23d5a27ce8
Mike Glenn <219478+ilude@users.noreply.github.com>
2024-04-27 09:16:37 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,17 +4,57 @@ set -e
 set -x
 set -o xtrace
 
-parted /dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi0 -- mklabel msdos
-parted /dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi0 -- mkpart primary 1MB -8GB
-parted /dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi0 -- mkpart primary linux-swap -8GB 100%
+DEVICE="/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi0"
+if [ ! -b "$DEVICE" ]; then
+  echo "ERROR: The default disk $DEVICE is missing!"
+  exit 1;
+fi
 
-mkfs.ext4 -L nixos /dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi0-part1
-mkswap -L swap /dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi0-part2
-swapon /dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi0-part2
-mount /dev/disk/by-label/nixos /mnt
-nixos-generate-config --root /mnt
+# List partitions on the device
+PARTITIONS=$(lsblk "$DEVICE" --output NAME --noheadings --raw)
 
-curl -s "https://raw.githubusercontent.com/ilude/nix/main/configuration.nix?$(date +%s)" > /mnt/etc/nixos/configuration.nix
-nano /mnt/etc/nixos/configuration.nix
+# Check if partitions exist
+if [ -n "$PARTITIONS" ]; then
+    echo "The device $DEVICE has the following partitions:"
+    echo "$PARTITIONS"
+else
+    parted $DEVICE -- mklabel msdos
+    parted $DEVICE -- mkpart primary 1MB -8GB
+    parted $DEVICE -- mkpart primary linux-swap -8GB 100%
+    
+    mkfs.ext4 -L nixos $DEVICE-part1
+    mkswap -L swap $DEVICE-part2
+    swapon /dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi0-part2
+    mount /dev/disk/by-label/nixos /mnt
+    nixos-generate-config --root /mnt
+fi
+
+# Check for a user password 
+if [ -z "${PW}" ]; then
+    # Prompt the user for a password
+    read -p "Enter User password: " -s PW
+    echo
+fi
+# Generate the hashed password
+HASHED_PASSWORD=$(mkpasswd "$PW")
+
+# download the configuation.nix template
+curl -s "https://raw.githubusercontent.com/ilude/nix/main/configuration.nix?$(date +%s)" > configuration.nix
+
+# process the template
+envsubst < configuration.nix > /mnt/etc/nixos/configuration.nix
 
 nixos-install
+
+while true; do
+    read -p "Do you want to reboot now? (y/n) " yn
+    case $yn in
+        [Yy]* )
+            reboot
+            break;;
+        [Nn]* )
+            exit;;
+        * )
+            echo "Please answer y or n.";;
+    esac
+done
