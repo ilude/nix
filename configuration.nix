a5f022ad52a201a4cbee022f4cca72f64069d323
Mike Glenn <219478+ilude@users.noreply.github.com>
2024-04-27 09:37:04 -0400

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -19,7 +19,7 @@
     isNormalUser = true;
     extraGroups = [ "wheel" "docker" "systemd-journal" ];
     shell = pkgs.zsh;
-    hashedPassword = "$HASHED_PASSWORD";
+    hashedPassword = "${HASHED_PASSWORD}";
     #packages = with pkgs; [];
   };
 
