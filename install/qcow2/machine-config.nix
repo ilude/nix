702ea2d1c0796a9f6a0e7d082d8d40d20fd7fd14
mike <219478+ilude@users.noreply.github.com>
2024-05-01 16:37:01 -0400

    working

--- install/qcow2/machine-config.nix
+++ install/qcow2/machine-config.nix
@@ -1,8 +1,7 @@
-{pkgs, lib, ... }:
-
-with lib;
-
-{
+{ pkgs, lib, ... }: let
+  # Import the secrets.nix file
+  secrets = import ./secrets.nix;
+in {
   imports = [
     <nixpkgs/nixos/modules/profiles/qemu-guest.nix>
   ];
@@ -19,7 +18,93 @@ with lib;
     boot.loader.grub.device = "/dev/vda";
     boot.loader.timeout = 0;
 
-    users.extraUsers.root.password = "";
+    nix.settings = {
+      auto-optimise-store = true;
+      experimental-features = [ "nix-command" "flakes" "repl-flake" ];
+    };
 
+    system.autoUpgrade.enable = true;
+
+    # Reference the username and password from secrets.nix
+    users.users."${secrets.user.username}" = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" "docker" "systemd-journal" ];
+      shell = pkgs.zsh;
+      hashedPassword = "${secrets.user.password}";
+    };
+
+    # Set the root's hashed password from secrets.nix
+    users.extraUsers.root = {
+      hashedPassword = "${secrets.root.password}";
+    };
+
+    security.sudo.wheelNeedsPassword = false;
+    users.defaultUserShell = pkgs.zsh;
+    environment.shells = [ pkgs.zsh ];
+
+    programs = {
+      zsh.enable = true;
+      nix-ld.enable = true;
+    };
+
+    networking.enableIPv6 = false;
+    i18n.defaultLocale = "en_US.UTF-8";
+
+    # Set your time zone.
+    time.timeZone = "America/New_York";
+    #time.timeZone = "America/Los_Angeles";
+    environment.variables = { TZ = "America/New_York"; };
+
+    services.openssh.enable = true;
+    services.qemuGuest.enable = true;
+    virtualisation.docker.enable = true;
+
+    environment.systemPackages = with pkgs; [
+      bash
+      cloud-init
+      coreutils
+      curl
+      docker
+      docker-buildx
+      docker-compose
+      eza
+      findutils
+      fzf
+      git
+      gnumake
+      gnutar
+      htop
+      iproute2
+      jq
+      just
+      killall
+      less
+      libuuid
+      linuxHeaders
+      mkpasswd
+      netcat
+      nettools
+      nixfmt
+      nixpkgs-fmt
+      nmap
+      openssl
+      python3
+      python3Packages.pip
+      ripgrep
+      rsync
+      spice-vdagent
+      ssh-import-id
+      strace
+      sysstat
+      tealdeer
+      tree
+      tzdata
+      unzip
+      util-linux
+      wget
+      yq
+      zsh-autosuggestions
+      zsh-syntax-highlighting
+    ];
   };
 }
---
+++ install/qcow2/secrets.nix
@@ -0,0 +1,9 @@
+{
+  user = {
+    username = "ilude";
+    password = "$6$Oe6kc9nip0cYOw6Q$hrHWU9robLw.qpV6.VUfQeWyP4BXalunpJaPG0OqFKi8U0rvMt67qj/dr6kf5pxqWXIFJs1rUrmrNp7FDs5HI.";
+  };
+  root ={
+    password = "$6$e9BEl4SezBRm8AOv$V.TP0s95H/kFk5isLaHLpOi8aYvICgS72JilMC.soWMIf8HUCLuIMRCVAwwrN5KeA8ANVyT.g3CMyUuhvGJ.B1";
+  };
+}
