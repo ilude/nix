bea3cd3f7ebb91e3dcad3b13a43f1c1b1e6c5b2a
mike <219478+ilude@users.noreply.github.com>
2024-05-01 23:01:14 -0400

    secrets in the configuration.nix

--- install/qcow2/build-qcow2.nix
+++ install/qcow2/build-qcow2.nix
@@ -16,4 +16,36 @@ with lib;
     format = "qcow2";
     configFile = pkgs.writeText "configuration.nix" (pkgs.lib.readFile ./machine-config.nix);
   };
+}{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+  secrets = import ./.secrets;
+
+  machineConfig = import ./machine-config.nix {
+    inherit pkgs lib;
+    secrets = secrets;
+  };
+
+  configFile = pkgs.writeText "configuration.nix" ''
+    { pkgs, lib, ... }:
+
+    ${builtins.toJSON machineConfig}
+  '';
+in
+{
+  imports = (machineConfig.config.imports or []) ++ [
+    <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
+  ];
+
+  config = machineConfig.config // {
+    system.build.qcow2 = import <nixpkgs/nixos/lib/make-disk-image.nix> {
+      inherit lib config;
+      pkgs = import <nixpkgs> { inherit (pkgs) system; };
+      diskSize = 8192;
+      format = "qcow2";
+      configFile = configFile;
+    };
+  };
 }
