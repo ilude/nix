752ffe001e46aa04d1ed397bcb3482b9d6e214f7
mike <219478+ilude@users.noreply.github.com>
2024-05-01 23:34:14 -0400

    sorta maybe kinda should propably

--- install/qcow2/build-qcow2.nix
+++ install/qcow2/build-qcow2.nix
@@ -2,32 +2,18 @@
 
 with lib;
 
-let
-  secrets = import ./.secrets;
-
-  machineConfig = import ./machine-config.nix {
-    inherit pkgs lib;
-    secrets = secrets;
-  };
-
-  configFile = pkgs.writeText "configuration.nix" ''
-    { pkgs, lib, ... }:
-
-    ${builtins.toJSON machineConfig}
-  '';
-in
 {
-  imports = (machineConfig.config.imports or []) ++ [
-    <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
-  ];
+  imports =
+    [ 
+      <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
+      ./machine-config.nix
+    ];
 
-  config = machineConfig.config // {
-    system.build.qcow2 = import <nixpkgs/nixos/lib/make-disk-image.nix> {
-      inherit lib config;
-      pkgs = import <nixpkgs> { inherit (pkgs) system; };
-      diskSize = 8192;
-      format = "qcow2";
-      configFile = configFile;
-    };
+  system.build.qcow2 = import <nixpkgs/nixos/lib/make-disk-image.nix> {
+    inherit lib config;
+    pkgs = import <nixpkgs> { inherit (pkgs) system; }; # ensure we use the regular qemu-kvm package
+    diskSize = 8192;
+    format = "qcow2";
+    configFile = pkgs.writeText "configuration.nix" (pkgs.lib.readFile ./machine-config.nix);
   };
 }
