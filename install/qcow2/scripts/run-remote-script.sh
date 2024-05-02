1817e503c3c4389165d008c214ba879b664ddbed
mike <219478+ilude@users.noreply.github.com>
2024-05-02 14:25:02 -0400

    more better

--- install/qcow2/scripts/run-remote-script.sh
+++ install/qcow2/scripts/run-remote-script.sh
@@ -1,24 +1,37 @@
 #!/usr/bin/env bash
-
 set -e
 
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <ssh_host> <ssh_command>"
-    exit 1
+if [ "$#" -lt 2 ] || [ "$#" -gt 3 ]; then
+  echo "Usage: $0 [-y] <ssh_host> <ssh_command>"
+  exit 1
+fi
+
+# Check if the -y flag is provided
+if [ "$1" == "-y" ]; then
+  SKIP_CONFIRM=true
+  shift
+else
+  SKIP_CONFIRM=false
 fi
 
 # Extract the hostname from the SSH host
 HOSTNAME=$(echo "$1" | cut -d "@" -f 2)
 
-# Prompt user for confirmation
-echo -e "\033[1;33mWARNING:\033[0;31m Running this command will destroy and create VM 8500 on $HOSTNAME.\033[0m"
-read -p "Are you sure you want to do this? (y/n): " choice
-case "$choice" in 
-  y|Y ) 
-    echo -e "\033[1;35mFuck it, We'll do it LIVE!\033[0m"
-    ssh "$1" "$2"
-    ;;
-  * ) 
-    echo "Action canceled."
-    ;;
-esac
+# Prompt user for confirmation if -y flag is not provided
+if [ "$SKIP_CONFIRM" = false ]; then
+  echo -e "\\033[1;33mWARNING:\\033[0;31m Running this command will destroy and create VM 8500 on $HOSTNAME.\\033[0m"
+  read -p "Are you sure you want to do this? (y/n): " choice
+  case "$choice" in
+    y|Y )
+      echo -e "\\033[1;35mFuck it, We'll do it LIVE!\\033[0m"
+      ;;
+    * )
+      echo "Action canceled."
+      exit 0
+      ;;
+  esac
+else
+  echo -e "\\033[1;35mFuck it, We'll do it LIVE!\\033[0m"
+fi
+
+ssh "$1" "$2"
